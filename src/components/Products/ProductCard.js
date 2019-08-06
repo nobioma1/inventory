53867b7bf297e95511b2d9f21f9bb6fb35b80fb5
Noble Obioma <nobioma1@gmail.com>
2019-08-06 15:59:43 +0100

    Create a product detail component

--- src/components/Products/ProductCard.js
+++ src/components/Products/ProductCard.js
@@ -1,86 +1,64 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import moment from 'moment';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { removeProduct, updateProduct } from '../../actions/product';
-import { DASHBOARD } from '../Routes/routes';
 
 const useStyles = makeStyles(theme => ({
   card: {
     maxWidth: 345,
     margin: theme.spacing(1),
   },
+  link: {
+    textDecoration: 'none',
+    color: '#3f51b5',
+  },
 }));
 
 const ProductCard = props => {
-  const { product, removeProduct, history } = props;
+  const { product } = props;
   const classes = useStyles();
-
-  const removeHandler = id => {
-    removeProduct(id).then(res => {
-      if (res) {
-        history.push(`${DASHBOARD}/${product.category}`);
-      }
-    });
-  };
 
   return (
     <React.Fragment>
       <Card className={classes.card}>
-        <CardActionArea>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              {product.name}
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              Serial: {product.serial}
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              Model: {product.model}
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              Category: {product.category}
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              Created At:
-              {moment(product.createdAt.toDate()).format(
-                'dddd, MMMM Do YYYY, h:mm a',
-              )}
-            </Typography>
-            {product.updatedAt && (
+        <Link className={classes.link} to={`${product.category}/${product.id}`}>
+          <CardActionArea>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {product.name}
+              </Typography>
               <Typography variant="body2" color="textSecondary" component="p">
-                Updated At:
-                {moment(product.updatedAt.toDate()).format(
+                Number of Products: {product.serials.length}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Model: {product.model}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Category: {product.category}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Created At:
+                {moment(product.createdAt.toDate()).format(
                   'dddd, MMMM Do YYYY, h:mm a',
                 )}
               </Typography>
-            )}
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button
-            size="small"
-            color="primary"
-            onClick={() =>
-              history.push(`${DASHBOARD}/${product.category}/${product.id}`)
-            }
-          >
-            Edit
-          </Button>
-          <Button
-            size="small"
-            color="secondary"
-            onClick={() => removeHandler(product.id)}
-          >
-            Delete
-          </Button>
-        </CardActions>
+              {product.updatedAt && (
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Updated At:
+                  {moment(product.updatedAt.toDate()).format(
+                    'dddd, MMMM Do YYYY, h:mm a',
+                  )}
+                </Typography>
+              )}
+            </CardContent>
+          </CardActionArea>
+        </Link>
       </Card>
     </React.Fragment>
   );
